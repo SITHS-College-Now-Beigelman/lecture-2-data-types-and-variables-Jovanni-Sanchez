89c45b14b8efb75e4341fddb902f34588a6b0ffc
Jovanni <jovannis8@nycstudents.net>
2024-09-16 16:24:20 -0400

    initial commit

---
+++ Sanchez_Jovanni_Lecture_2.cpp
@@ -0,0 +1,25 @@
+// Jovanni Sanchez
+// Lecture 2
+// September 16, 2024
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int wholeNum = 24;                     // Intenger (Whole Number)
+    float floatingNum = 0.5;              // Floating point number
+    double doubleTrouble = 66.6;         // Floating point number (But BIGGER)
+    char singleLetter = 'A';            // Character
+    bool trueOrFalse = true;           // Boolean
+    string myFinalWords = "WOW! :O";  // String
+
+    cout << "int: " << wholeNum << "\n";
+    cout << "float: " << floatingNum << "\n";
+    cout << "double: " << doubleTrouble << "\n";
+    cout << "char: " << singleLetter << "\n";
+    cout << "bool: " << trueOrFalse << "\n";
+    cout << "string: " << myFinalWords << "\n";
+
+    return 0;
+}
