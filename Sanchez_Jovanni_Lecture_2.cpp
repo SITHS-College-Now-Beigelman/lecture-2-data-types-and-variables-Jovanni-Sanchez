b7f6297d38151acab50de7090701fb8ca4b4ba5f
Jovanni <jovannis8@nycstudents.net>
2024-09-17 15:27:23 -0400

    i think that is it

--- Sanchez_Jovanni_Lecture_2.cpp
+++ Sanchez_Jovanni_Lecture_2.cpp
@@ -7,6 +7,8 @@
 using namespace std;
 
 int main(){
+    /* Outputting variables */
+
     int wholeNum = 24;                     // Intenger (Whole Number)
     float floatingNum = 0.5;              // Floating point number
     double doubleTrouble = 66.6;         // Floating point number (But BIGGER)
@@ -14,12 +16,32 @@ int main(){
     bool trueOrFalse = true;           // Boolean
     string myFinalWords = "WOW! :O";  // String
 
-    cout << "int: " << wholeNum << "\n";
-    cout << "float: " << floatingNum << "\n";
+    cout << "int: "    << wholeNum      << "\n";
+    cout << "float: "  << floatingNum   << "\n";
     cout << "double: " << doubleTrouble << "\n";
-    cout << "char: " << singleLetter << "\n";
-    cout << "bool: " << trueOrFalse << "\n";
-    cout << "string: " << myFinalWords << "\n";
+    cout << "char: "   << singleLetter  << "\n";
+    cout << "bool: "   << trueOrFalse   << "\n";
+    cout << "string: " << myFinalWords  << "\n";
+
+    /* Arithmetic Operators (From Slide 37) */
+
+    int inches = 100;
+
+    cout << inches      << " inch(es) = ";
+    cout << inches / 12 << " feet (foot) and ";
+    cout << inches % 12 << " inch(es)" << endl;
+
+    /* Comparing ASCII characters */
+
+    char letterZ = 'Z';
+
+    cout << letterZ << " > " << singleLetter << "\n";
+
+    /* Evaluating mixed equations */
+
+    cout << "2 + 3.5 = " << 2 + 3.5 << "\n";
+    cout << "6 / 4 + 3.9 = " << 6 / 4 + 3.9 << "\n";
+    cout << "5.4 * 2 - (13.6 + 18 / 2) = " << 5.4 * 2 - (13.6 + 18 / 2) << "\n";
 
     return 0;
 }
